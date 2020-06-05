c7e3ca41ff1547eb703dfc8eba15a6a1e619344b
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2020-06-05 15:44:24 +0200

    Creating additional Tenants controller

---
+++ ResourceManager/Controllers/ITenantsController.cs
@@ -0,0 +1,33 @@
+﻿using ResourceManager.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourceManager.Controllers
+{
+    /// <summary>
+    /// Interfejs kontrolera, którego zadaniem jest zarządanie dzierżawcami dostępnymi dla aplikacji
+    /// </summary>
+    public interface ITenantsController
+    {
+        /// <summary>
+        /// Dodaje dzierżawcę do puli
+        /// </summary>
+        /// <param name="Tenant">Nowy dzierżawca</param>
+        void AddTenant(ITenant Tenant);
+
+        /// <summary>
+        /// Usuwa dzierżawcę z puli
+        /// </summary>
+        /// <param name="Id"></param>
+        void RemoveTenant(Guid Id);
+
+        /// <summary>
+        /// Zmienia priorytet dzierżawcy, np. w przypadku gdy ten wykupił droższy abonament, lub odwrotnie.
+        /// </summary>
+        /// <param name="Id">ID dzierżawcy</param>
+        /// <param name="NewPiority">Nowy priorytet dla dzierżawcy</param>
+        void SetTenantsPriority(Guid Id, byte NewPiority);
+    }
+}
---
+++ ResourceManager/Controllers/TenantsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResourceManager.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourceManager.Controllers
+{
+    /// <summary>
+    /// Implementacja kontrolera dzierżawców
+    /// </summary>
+    public class TenantsController : Controller, ITenantsController
+    {
+        public void AddTenant(ITenant Tenant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveTenant(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetTenantsPriority(Guid Id, byte NewPiority)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
