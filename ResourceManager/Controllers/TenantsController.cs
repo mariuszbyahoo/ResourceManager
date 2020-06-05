d16a7acc0ecde47639b631c19b59bb0a5430df8c
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2020-06-05 19:06:39 +0200

    Finishing TenantsController

--- ResourceManager/Controllers/TenantsController.cs
+++ ResourceManager/Controllers/TenantsController.cs
@@ -28,19 +28,25 @@ namespace ResourceManager.Controllers
         [Route("add")]
         public ActionResult<ITenant> AddTenantAction([FromBody]Tenant Tenant)
         {
-            // W przypadku dostarczenia nieprawidłowego GUID, obiekt "Tenant" będzie referencją do null
+            // Jeśli podane dane są nieprawidłowe, będzie referencją do null.
             if (Tenant == null)
                 return BadRequest("Incorrect data provided");
 
-            var lookup = _ctx.Tenants.Where(tenant => tenant.Id.Equals(Tenant.Id)).FirstOrDefault();
+            var lookup = GetTenantById(Tenant.Id);
 
+            // Czy istnieje już Tenant z takim ID
             if (lookup != null)
                 return BadRequest("Tenant with such an ID already exists");
 
             AddTenant(Tenant);
-            return Created("addTenant", Tenant);
+
+            return Created("add", Tenant);
         }
         
+        /// <summary>
+        /// Metoda wywoływana przez akcję kontrolera po sprawdzeniu błędnych scenariuszy
+        /// </summary>
+        /// <param name="Tenant"></param>
         public void AddTenant(ITenant Tenant)
         {
             var tenant = _factory.CreateInstance(Tenant.Id, Tenant.Priority, "Tenant") as Tenant;
@@ -48,14 +54,56 @@ namespace ResourceManager.Controllers
             _ctx.SaveChanges();
         }
 
-        public void RemoveTenant(Guid Id)
+        [HttpDelete]
+        [Route("delete/{Id}")]
+        public ActionResult RemoveTenantAction(Guid Id)
         {
-            throw new NotImplementedException();
+            if (GetTenantById(Id) == null)
+                return BadRequest("Such a Tenant is missing.");
+
+            RemoveTenant(Id);
+
+            _ctx.SaveChanges();
+
+            return NoContent();
         }
 
-        public void SetTenantsPriority(Guid Id, byte NewPiority)
+        /// <summary>
+        /// Metoda wywoływana przez akcję kontrolera po sprawdzeniu błędnych scenariuszy
+        /// </summary>
+        /// <param name="Id"></param>
+        public void RemoveTenant(Guid Id)
         {
-            throw new NotImplementedException();
+            _ctx.Tenants.Remove(GetTenantById(Id));
+            _ctx.SaveChanges();
+        }
+
+        [HttpPatch]
+        [Route("patch")]
+        public ActionResult SetTenantsPriorityAction([FromBody] Tenant tenant)
+        {
+            var tenantFromDb = GetTenantById(tenant.Id);
+            if (tenantFromDb == null)
+                return BadRequest("Such a tenant is missing");
+
+            SetTenantsPriority(tenant.Id, tenant.Priority);
+
+            return Ok($"Tenant with an ID of: {tenant.Id} has been updated with priority of: {tenant.Priority}");
+        }
+
+        public void SetTenantsPriority(Guid Id, byte NewPriority)
+        {
+            // ciągłe powtarzanie użycia metody GetTenantById wymuszone przez z góry ustalone w wymaganiach argumenty do metod interfejsu.
+            var tenant = GetTenantById(Id);
+            tenant.Priority = NewPriority;
+            _ctx.Update(tenant);
+            _ctx.SaveChanges();
+        }
+
+        private Tenant GetTenantById(Guid Id)
+        {
+            return _ctx.Tenants.Where(t => t.Id.Equals(Id)).FirstOrDefault();
+
         }
     }
 }
