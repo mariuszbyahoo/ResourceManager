40cfdfd1bc4931d65a764c73a0855fd4a168d883
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2020-06-06 13:28:05 +0200

    removing FetchHelper class

--- ResourceManager/Startup.cs
+++ ResourceManager/Startup.cs
@@ -4,15 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using ResourceManager.Data;
 using ResourceManager.Data.Repos;
-using ResourceManager.Domain;
 using ResourceManager.Domain.Factories;
 
 namespace ResourceManager
@@ -26,7 +23,6 @@ namespace ResourceManager
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -37,13 +33,10 @@ namespace ResourceManager
             services.AddTransient<ITenantRepo, TenantRepo>();
             services.AddTransient<IResourceRepo, ResourceRepo>();
 
-            // TODO, mog¹ byæ do usuniêcia
             services.AddSingleton<IResourceFactory, ResourceFactory>();
             services.AddSingleton<ITenantsFactory, TenantsFactory>();
-            services.AddSingleton<IFetchHelper, FetchHelper>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
