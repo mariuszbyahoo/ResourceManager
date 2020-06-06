467bb5457963486e7717ed8afbf59388e695b242
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2020-06-06 13:00:18 +0200

    Adding ResourceRepo

---
+++ ResourceManager.Data/Repos/IResourceRepo.cs
@@ -0,0 +1,23 @@
+﻿using ResourceManager.Domain.Factories;
+using ResourceManager.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResourceManager.Data.Repos
+{
+    public interface IResourceRepo
+    {
+        public ManagerDbContext Ctx { get; set; }
+        Resource GetResource(Guid id);
+        Resource[] GetResources(string variant);
+        Resource[] GetAvailableResources(Resource[] resources);
+
+        void AddResource(IResource resource, DateTime availableFromDate);
+        void WithdrawResource(IResource resource, DateTime withdrawedOnDate);
+        bool FreeResource(IResource resource, ITenant tenant, DateTime date);
+        bool LeaseResource(IResource resource, ITenant tenant, DateTime date);
+        IResource LeaseResource(string variant, ITenant tenant, DateTime date);
+        
+    }
+}
---
+++ ResourceManager.Data/Repos/ResourceRepo.cs
@@ -0,0 +1,87 @@
+﻿using ResourceManager.Domain.Enums;
+using ResourceManager.Domain.Factories;
+using ResourceManager.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ResourceManager.Data.Repos
+{
+    public class ResourceRepo : IResourceRepo
+    {
+        public ManagerDbContext Ctx { get; set; }
+
+        public ResourceRepo(ManagerDbContext ctx)
+        {
+            Ctx = ctx;
+        }
+
+        public void AddResource(IResource resource, DateTime availableFromDate)
+        {
+            // TODO Handle dates!
+            Ctx.Resources.Add(resource as Resource);
+            Ctx.SaveChanges();
+        }
+
+        public bool FreeResource(IResource resource, ITenant tenant, DateTime date)
+        {
+            resource.Availability = Domain.Enums.ResourceStatus.Available;
+            resource.LeasedTo = Guid.Empty;
+            Ctx.Update(resource);
+            Ctx.SaveChanges();
+            return true;
+        }
+
+        public Resource[] GetAvailableResources(Resource[] resources)
+        {
+            return resources.Where(r => r.Availability.Equals(ResourceStatus.Available)).ToArray();
+        }
+
+        public Resource GetResource(Guid id)
+        {
+            // TODO handle this exception
+            // Throws error if duplicate
+            return Ctx.Resources.Where(r => r.Id.Equals(id)).Single();
+        }
+
+        public Resource[] GetResources(string variant)
+        {
+            return Ctx.Resources.Where(r => r.Variant.ToLower().Equals(variant.ToLower())).ToArray();
+        }
+
+        public bool LeaseResource(IResource resource, ITenant tenant, DateTime date)
+        {
+            resource.Availability = Domain.Enums.ResourceStatus.Occupied;
+            resource.LeasedTo = tenant.Id;
+            Ctx.Update(resource);
+            Ctx.SaveChanges();
+            return true;
+        }
+
+        public IResource LeaseResource(string variant, ITenant tenant, DateTime date)
+        {
+            IResource resource;
+            var resources = GetResources(variant);
+            if (resources.Length == 0)
+                return null;
+
+            // Wybierz ten, który najdłużej leży odłogiem i jest wolny
+            resource = GetAvailableResources(resources).FirstOrDefault();
+
+            resource.Availability = ResourceStatus.Occupied;
+            resource.LeasedTo = tenant.Id;
+
+            Ctx.Update(resource);
+            Ctx.SaveChanges();
+            return resource;
+        }
+
+        public void WithdrawResource(IResource resource, DateTime withdrawedOnDate)
+        {
+            // TODO handle the dates
+            Ctx.Resources.Remove(resource as Resource);
+            Ctx.SaveChanges();
+        }
+    }
+}
