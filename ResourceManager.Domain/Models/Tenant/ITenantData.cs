e347b4ce88747ab24c942ff0d1ac037cfd302673
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2020-06-07 17:45:29 +0200

    Creating new Models for DataService

---
+++ ResourceManager.Domain/Models/Tenant/ITenantData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResourceManager.Domain.Models
+{
+    public interface ITenantData
+    {
+        string EmailAddress { get; set; }
+    }
+}
---
+++ ResourceManager.Domain/Models/Tenant/TenantData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResourceManager.Domain.Models
+{
+    public class TenantData : ITenantData
+    {
+        public string EmailAddress { get; set; }
+    }
+}
