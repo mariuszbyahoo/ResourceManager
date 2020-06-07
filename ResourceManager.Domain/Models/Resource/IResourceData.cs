1bdeba73b33ba41472ce8b8cf930315ecafe479d
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2020-06-07 17:49:18 +0200

    Adding ResourceData entity

---
+++ ResourceManager.Domain/Models/Resource/IResourceData.cs
@@ -0,0 +1,25 @@
+﻿using ResourceManager.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResourceManager.Domain.Models
+{
+    public interface IResourceData
+    {
+		/// <summary>
+		/// Status zasobu; dostępny i zajęty.
+		/// </summary>
+		ResourceStatus Availability { get; set; }
+
+		/// <summary>
+		/// Identyfikator dzierżawcy 
+		/// </summary>
+		Guid LeasedTo { get; set; }
+
+		/// <summary>
+		/// Data, do której dany zasób jest niedostępny
+		/// </summary>
+		public DateTime OccupiedTill { get; set; }
+	}
+}
---
+++ ResourceManager.Domain/Models/Resource/ResourceData.cs
@@ -0,0 +1,14 @@
+﻿using ResourceManager.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResourceManager.Domain.Models
+{
+    public class ResourceData : IResourceData
+    {
+        public ResourceStatus Availability { get; set; }
+        public Guid LeasedTo { get; set; }
+        public DateTime OccupiedTill { get; set; }
+    }
+}
